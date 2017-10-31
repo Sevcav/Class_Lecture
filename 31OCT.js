6908956946fd0f14dda7ba8a6ed6d6205117e457
KLC <32318793+Sevcav@users.noreply.github.com>
2017-10-31 12:36:35 -0700

    final

--- 31OCT.js
+++ 31OCT.js
@@ -14,3 +14,22 @@ var counter = {
 for (counter.reset(10) ; counter.count() > 0; counter.inc(-1)) {
   console.log("current value: " + counter.count());
 }
+
+function incrementer(incAmount) {
+    this.num = 0;
+    this.amount = incAmount;
+    this.inc = function(){
+        this.num = this.num + this.amount;
+    };
+    this.value = function(){
+        return this.num;
+    };
+}
+
+var inc1 = new incrementer(2);
+var inc2 = new incrementer(1);
+var inc3 = new incrementer(10);
+
+inc1.inc(); inc1.inc(); console.log(inc1.value());
+console.log(inc2.value());
+inc3.inc; console.log(inc3());
